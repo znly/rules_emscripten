fb958861281da2d243618bf1e4ae8315482372c7
Steeve Morin <steeve@zen.ly>
2018-11-14 20:01:34 +0100

    toolchain: make emar support .lo files
    When using alwayslink in cc_library, bazel will produce .lo files and
    pass --whole-archive to the linker. While this is fine in theory,
    emar.py will add fullpath hashes to the filename in the archive to
    prevent basenames collisions. However, it only does it if the archive
    has a .a extension. This commit makes it so that the output name passed
    to emar.py is a .a extension, which is then moved to the original .lo
    file.
    
    Signed-off-by: Steeve Morin <steeve@zen.ly>

--- toolchain/emar.sh
+++ toolchain/emar.sh
@@ -4,4 +4,17 @@ set -euo pipefail
 
 source $(dirname $0)/emenv.sh
 
-python external/emscripten_toolchain/emar.py "$@"
+argv=("${@}")
+outfile=${argv[1]}
+outarchive=
+
+if [[ ${outfile} == *.lo ]]; then
+    outarchive=${outfile%.*}.a
+    argv[1]=${outarchive}
+fi
+
+python external/emscripten_toolchain/emar.py "${argv[@]}"
+
+if [[ ! -z "${outarchive}" ]]; then
+    mv "${outarchive}" "${outfile}"
+fi
