d4ba595f9331505ee12955cb5ae8541e5afbaf2d
Steeve Morin <steeve@zen.ly>
2018-11-16 12:33:25 +0100

    toolchain: use the real sandbox path to ensure emscripten find its tools
    Signed-off-by: Steeve Morin <steeve@zen.ly>

--- toolchain/emenv.sh
+++ toolchain/emenv.sh
@@ -2,18 +2,28 @@
 
 set -euo pipefail
 
-export EMCC_TEMP_DIR="${PWD}/emtmp"
+cwd="${PWD}"
+
+# Somehow emar will fail to find llvm-ar when using the /proc/self/cwd path. To
+# prevent that, on linux, we find out the "real path" of the sandbox. We do not
+# rely on realpath as it may not be installed on the machine. We could compile
+# one ourselves, but it's not really nescessary.
+if [[ "${OSTYPE}" == "linux-gnu" ]]; then
+    cwd="$(readlink ${cwd})"
+fi
+
+export EMCC_TEMP_DIR="${cwd}/emtmp"
 mkdir -p ${EMCC_TEMP_DIR}
 trap "rm -rf ${EMCC_TEMP_DIR}" EXIT
 
-EM_CONFIG="LLVM_ROOT='${PWD}/external/emscripten_clang';"
+EM_CONFIG="LLVM_ROOT='${cwd}/external/emscripten_clang';"
 EM_CONFIG+="EMSCRIPTEN_NATIVE_OPTIMIZER='external/emscripten_clang/optimizer';"
 EM_CONFIG+="BINARYEN_ROOT='external/emscripten_clang/binaryen';"
 EM_CONFIG+="EMSCRIPTEN_ROOT='external/emscripten_toolchain';"
 EM_CONFIG+="TEMP_DIR='${EMCC_TEMP_DIR}';"
 EM_CONFIG+="SPIDERMONKEY_ENGINE='';"
 EM_CONFIG+="V8_ENGINE='';"
-EM_CONFIG+="NODE_JS='${PWD}/external/nodejs/bin/node';"
+EM_CONFIG+="NODE_JS='${cwd}/external/nodejs/bin/node';"
 EM_CONFIG+="COMPILER_ENGINE=NODE_JS;"
 EM_CONFIG+="JS_ENGINES=[NODE_JS];"
 export EM_CONFIG
