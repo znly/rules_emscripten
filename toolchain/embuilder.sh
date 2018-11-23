42bc38c59bf7e4a9fab0df641336ea62372d9788
Steeve Morin <steeve@zen.ly>
2018-11-23 14:14:55 +0100

    toolchain: rework embuilder runfiles and cache generation
    Signed-off-by: Steeve Morin <steeve@zen.ly>

--- toolchain/embuilder.sh
+++ toolchain/embuilder.sh
@@ -2,8 +2,28 @@
 
 set -euo pipefail
 
-toolchain_dir="$(dirname $0)"
-source ${toolchain_dir}/emenv.sh
+# --- begin runfiles.bash initialization ---
+if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+    if [[ -f "$0.runfiles_manifest" ]]; then
+      export RUNFILES_MANIFEST_FILE="$0.runfiles_manifest"
+    elif [[ -f "$0.runfiles/MANIFEST" ]]; then
+      export RUNFILES_MANIFEST_FILE="$0.runfiles/MANIFEST"
+    elif [[ -f "$0.runfiles/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+      export RUNFILES_DIR="$0.runfiles"
+    fi
+fi
+if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+  source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
+elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
+            "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
+else
+  echo >&2 "ERROR: cannot find @bazel_tools//tools/bash/runfiles:runfiles.bash"
+  exit 1
+fi
+# --- end runfiles.bash initialization ---
+
+source "$(rlocation rules_emscripten/toolchain/emenv.sh)"
 
 cflags=(
     "-isystem"
@@ -26,9 +46,13 @@ cflags=(
 
 export EM_CACHE="${BUILD_WORKSPACE_DIRECTORY}/toolchain/emscripten_cache"
 export EMCC_CFLAGS="${cflags[@]}"
+
+# Start by removing the cache, before rebuilding
+rm -rf "${EM_CACHE}/asmjs"
 python external/emscripten_toolchain/embuilder.py "$@"
 rm -rf "${EM_CACHE}/asmjs/ports-builds"
 
-# # Remove the first line of .d file (emscripten resisted all my attempts to make
-# # it realize it's just the absolute location of the source)
-find . -name '*.d' -exec ${toolchain_dir}/sed_i.sh '2d' {} \;
+# Remove the first line of .d file (emscripten resisted all my attempts to make
+# it realize it's just the absolute location of the source)
+sed_i="$(rlocation rules_emscripten/toolchain/sed_i.sh)"
+find . -name '*.d' -exec "${sed_i}" '2d' {} \;
